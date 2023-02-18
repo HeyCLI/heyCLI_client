b7698dbebf74f30efa66a26e06eed83d53442260
Hadiazzouni <a_azouni@esi.dz>
2023-02-18 23:59:21 +0100

    Update heycli_client.sh

--- heycli_client.sh
+++ heycli_client.sh
@@ -18,12 +18,35 @@ function hey() {
   recent_history=~/.recent_hist.txt 
   cmd="$*"
   current_dir="$(pwd)"
-  curl -s \
+
+  result=$(curl -s \
                 -F "command=$cmd" \
                 -F "recent_history=@$recent_history" \
                 -H "Authorization:$HEYCLI_API_KEY" \
                 -F "curr_dir=$current_dir" \
-		https://api.heycli.com/heycli
+		    https://api.heycli.com/heycli)
+  
+  echo "$result"
+  
+  command_line=$(echo "$result" | grep -o "Answer: .*" | sed 's/Answer: //')
+
+  # echo "Command is: $command_line"
+
+  if [[ -n "$command" ]]; then 
+      echo -n "Execute command? (yes/no): "  # prompt user to confirm choice
+      read choice   # read user input
+            case "$choice" in
+                [Yy][Ee][Ss])   # if user enters "yes"
+                    eval "$command_line"   # execute the command
+                    ;;
+                [Nn][Oo])   # if user enters "no"
+                    echo "Command not executed."
+                    ;;
+                *)   # if user enters anything else
+                    echo "Please enter 'yes' or 'no'."
+                    ;;
+            esac
+  fi
                 
 }
 
@@ -33,10 +56,11 @@ function hey() {
 function save_output {
 
   # run the command and capture its output
-  output=$($@ 2>&1 | tee ~/.command_outputs)
+  # output=$($@ 2>&1 | tee ~/.command_outputs)
+  output=$($@ 2>&1 | tee /dev/tty)
 
   # display the output
-  echo "$output"
+  # echo "$output"
   command="$*"
   current_dir="$(pwd)"
   # send the output to an API endpoint in a background process
@@ -50,7 +74,10 @@ function save_output {
 		
 }
 
+
+
 # alias the function to every command
+alias ls='save_output ls'
 alias sudo='save_output sudo'
 alias ssh='save_output ssh'
 alias ping='save_output ping'
